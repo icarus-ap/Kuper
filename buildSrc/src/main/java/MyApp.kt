6c3cf515d9c0dd17e4ba1868f6f43e8814ab5c71
IcarusAP <65499301+icarus-ap@users.noreply.github.com>
2024-07-31 17:12:24 +0530

    updated to 138

--- buildSrc/src/main/java/MyApp.kt
+++ buildSrc/src/main/java/MyApp.kt
@@ -2,6 +2,6 @@
 
 object MyApp {
     const val appId = "com.icarusap.uzuri2"
-    const val version = 137
-    const val versionName = "1.3.7"
+    const val version = 138
+    const val versionName = "1.3.8"
 }
